7cf7b07f4b862ae86833b356a0ea54293df102a7
EmmaCaldwell <emmacaldwell@gmx.de>
2021-12-03 11:56:32 +0100

    Update run_tatoeba.sh

--- scripts/run_tatoeba.sh
+++ scripts/run_tatoeba.sh
@@ -14,7 +14,7 @@
 # limitations under the License.
 
 REPO=$PWD
-MODEL=${1:-bert-base-multilingual-cased}
+MODEL=${1:-bert-base-multilingual-based}
 GPU=${2:-0}
 DATA_DIR=${3:-"$REPO/download/"}
 OUT_DIR=${4:-"$REPO/outputs/"}
@@ -33,33 +33,92 @@ if [ $MODEL == "bert-base-multilingual-cased" ]; then
 elif [ $MODEL == "xlm-mlm-100-1280" ] || [ $MODEL == "xlm-mlm-tlm-xnli15-1024" ]; then
   MODEL_TYPE="xlm"
   LC=" --do_lower_case"
-  DIM=1280
+  if [ $MODEL == "xlm-mlm-100-1280" ]; then
+    DIM=1280
+  elif [ $MODEL == "xlm-mlm-tlm-xnli15-1024" ]; then
+    DIM=1024
+  fi
 elif [ $MODEL == "xlm-roberta-large" ] || [ $MODEL == "xlm-roberta-base" ]; then
   MODEL_TYPE="xlmr"
-  DIM=1024
+  if [ $MODEL == "xlm-roberta-large" ]; then
+    DIM=1024
+  elif [ $MODEL == "xlm-roberta-base" ]; then
+    DIM=768
+  NLAYER=24
+  LAYER=13
+  fi
+elif [ $MODEL == "google/mt5-small" ] || [ $MODEL == "google/mt5-xxl" ]; then
+  MODEL_TYPE="mt5"
+  DIM=512
+  NLAYER=24
+  LAYER=13
+elif [ $MODEL == "rembert" ]; then
+  MODEL_TYPE="rembert"
+  DIM=256
   NLAYER=24
   LAYER=13
 fi
 
 # Add fine-tuned model path here
-#MODEL=/mnt/disk-1/models/squad/bert-base-multilingual-cased
+#MODEL=/mnt/disk-1/models/squad/bert-base-multilingual-cased_LR?_EPOCH?_maxlen?_batchsize?_gradacc?
 
 OUT=$OUT_DIR/$TASK/${MODEL}_${MAXL}/
+#OUT=/content/gdrive/MyDrive/xtreme-master
 mkdir -p $OUT
-for SL in ar he vi id jv tl eu ml ta te af nl en de el bn hi mr ur fa fr it pt es bg ru ja ka ko th sw zh kk tr et fi hu az lt pl uk ro; do
-  python $REPO/third_party/evaluate_retrieval_mT5.py \
-    --model_type $MODEL_TYPE \
-    --model_name_or_path $MODEL \
-    --embed_size $DIM \
-    --batch_size 100 \
-    --task_name $TASK \
-    --src_language $SL \
-    --tgt_language en \
-    --data_dir $DATA_DIR/$TASK/ \
-    --max_seq_length $MAXL \
-    --output_dir $OUT \
-    --log_file embed-cosine \
-    --num_layers $NLAYER \
-    --dist cosine $LC \
-    --specific_layer $LAYER
-done
+if [ $MODEL=="bert-base-multilingual-cased" ] || [ $MODEL=="xlm-mlm-100-1280" ] || [ $MODEL=="xlm-mlm-tlm-xnli15-1024" ] || [ $MODEL=="xlm-roberta-large" ] || [ $MODEL=="xlm-roberta-base" ]; then
+  for SL in ar he vi id jv tl eu ml ta te af nl en de el bn hi mr ur fa fr it pt es bg ru ka ko th sw zh kk tr et fi hu az lt pl uk ro; do
+    python $REPO/third_party/evaluate_retrieval.py \
+      --model_type $MODEL_TYPE \
+      --model_name_or_path $MODEL \
+      --embed_size $DIM \
+      --batch_size 100 \
+      --task_name $TASK \
+      --src_language $SL \
+      --tgt_language en \
+      --data_dir $DATA_DIR/$TASK/ \
+      --max_seq_length $MAXL \
+      --output_dir $OUT \
+      --log_file embed-cosine \
+      --num_layers $NLAYER \
+      --dist cosine $LC \
+      --specific_layer $LAYER
+  done
+    
+elif [ $MODEL == "google/mt5-small" ] || [ $MODEL == "google/mt5-xxl" ]; then
+  for SL in ar he vi id jv tl eu ml ta te af nl en de el bn hi mr ur fa fr it pt es bg ru ka ko th sw zh kk tr et fi hu az lt pl uk ro; do
+    python $REPO/third_party/evaluate_retrieval_mT5.py \
+      --model_type $MODEL_TYPE \
+      --model_name_or_path $MODEL \
+      --embed_size $DIM \
+      --batch_size 100 \
+      --task_name $TASK \
+      --src_language $SL \
+      --tgt_language en \
+      --data_dir $DATA_DIR/$TASK/ \
+      --max_seq_length $MAXL \
+      --output_dir $OUT \
+      --log_file embed-cosine \
+      --num_layers $NLAYER \
+      --dist cosine $LC \
+      --specific_layer $LAYER
+  done
+      
+elif [ $MODEL == "rembert" ]; then
+  for SL in ar he vi id jv tl eu ml ta te af nl en de el bn hi mr ur fa fr it pt es bg ru ka ko th sw zh kk tr et fi hu az lt pl uk ro; do
+    python $REPO/third_party/evaluate_retrieval_rembert.py \
+      --model_type $MODEL_TYPE \
+      --model_name_or_path $MODEL \
+      --embed_size $DIM \
+      --batch_size 100 \
+      --task_name $TASK \
+      --src_language $SL \
+      --tgt_language en \
+      --data_dir $DATA_DIR/$TASK/ \
+      --max_seq_length $MAXL \
+      --output_dir $OUT \
+      --log_file embed-cosine \
+      --num_layers $NLAYER \
+      --dist cosine $LC \
+      --specific_layer $LAYER
+  done
+fi
